58bef34cabebf01573cd96e266bfe18fd6b303d8
nhorvath <nhorvath@gmail.com>
2015-02-27 14:23:39 -0500

    $@

---
+++ update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $# -lt 1 ] ; then
+  echo "Usage $0 [commit message]"
+  echo "This will pull from master repo and rebuild php.js"
+  exit 1
+fi
+
+git fetch main
+git merge main/master
+git push
+cd tools
+./phpjscompile.php > ../build/php.js
+cd ..
+git add .
+git commit -m '$@' .
+git push
